ae46f5091f630857730cf58dc65575577f99c666
Patrick Howard <patrickwhoward@pm.me>
2023-07-04 22:00:30 -0500

    undid the packer stuff, hopefully not necessary with nvchad. added some new requirements to lspconfig

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -4,7 +4,6 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require("lspconfig")
 -- local servers = { "html", "cssls", "clangd", "cmake" }
 local cmp_nvim_lsp = require("cmp_nvim_lsp")
-local mason_lspconfig = require("mason-lspconfig")
 
 lspconfig.clangd.setup({
 	on_attach = on_attach,
@@ -21,8 +20,14 @@ lspconfig.cmake.setup({
 	on_attach = on_attach,
 	capabilities = capabilities,
 })
-mason_lspconfig.setup({
-	ensure_installed = { "lua_ls", "clangd", "dockerls", "cmake", "jsonls" },
+lspconfig.dockerls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+
+lspconfig.jsonls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
 })
 -- This for loop will automatically set everything up for me, but I need to manually control the command for clang
 -- for _, lsp in ipairs(servers) do
