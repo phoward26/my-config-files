15b6d4b3dc8d0a8df0f509222d9c6001a2f82940
Patrick Howard <patrickwhoward@pm.me>
2023-07-04 22:06:14 -0500

    did mason packages the correct way

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -1,31 +1,47 @@
 local plugins = {
-  {
-    "neovim/nvim-lspconfig",
+	{
+		"neovim/nvim-lspconfig",
 
-    dependencies = {
-      "jose-elias-alvarez/null-ls.nvim",
-      config = function()
-        require "custom.configs.null-ls"
-      end,
-    },
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "vim",
-        "lua",
+		dependencies = {
+			"jose-elias-alvarez/null-ls.nvim",
+			config = function()
+				require("custom.configs.null-ls")
+			end,
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"vim",
+				"lua",
 
-        "cpp",
-        "cmake",
+				"cpp",
+				"cmake",
+				"json",
+				"dockerfile",
+			},
+		},
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"lua-language-server",
+				"html-lsp",
+				"prettier",
+				"stylua",
+        "neocmake",
+        "clangd",
         "json",
-        "dockerfile"
-      }
-    }
-  }
+        "dockerls",
+        "docker_compose_language_service",
+			},
+		},
+	},
 }
 return plugins
